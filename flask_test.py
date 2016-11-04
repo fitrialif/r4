fa2be39ed2a085489a12dfe6fbff8d362a82b874
hiepph <hoanghiepjp96@gmail.com>
2016-11-04 23:40:21 +0700

    test: POST request with various headers

--- flask_test.py
+++ flask_test.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request, json
 
 app = Flask(__name__)
 
@@ -13,6 +13,24 @@ def api_hello():
         return 'Hello ' + request.args['name']
     else:
         return 'Hello'
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type :("
 
 if __name__ == '__main__':
     app.run(debug=True)
