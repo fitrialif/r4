aaad170a9016f1f1756ee95ab123f97e5f6fd75e
hiepph <hoanghiepjp96@gmail.com>
2016-11-06 12:05:40 +0000

    feat(dc): Finish the 4-basic dc movement

--- brain/dc.py
+++ brain/dc.py
@@ -11,6 +11,14 @@ GPIO.setup(Motor1E, GPIO.OUT)
 GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 
+Motor2E = 22
+Motor2A = 24
+Motor2B = 26
+
+GPIO.setup(Motor2E, GPIO.OUT)
+GPIO.setup(Motor2A, GPIO.OUT)
+GPIO.setup(Motor2B, GPIO.OUT)
+
 
 def c1():
     """
@@ -31,23 +39,63 @@ def cc1():
 
 
 def c2():
-    pass
+    """
+    Move the motor 2 clcockwise
+    """
+    GPIO.output(Motor2E, GPIO.HIGH)
+    GPIO.output(Motor2A, GPIO.HIGH)
+    GPIO.output(Motor2B, GPIO.LOW)
 
 
 def cc2():
-    pass
+    """
+    Move the motor 2 counter-clockwise
+    """
+    GPIO.output(Motor2E, GPIO.HIGH)
+    GPIO.output(Motor2A, GPIO.LOW)
+    GPIO.output(Motor2B, GPIO.HIGH)
+
+
+def stop1():
+    GPIO.output(Motor1E, GPIO.LOW)
+
+
+def stop2():
+    GPIO.output(Motor2E, GPIO.LOW)
 
 
 def stop():
-    GPIO.output(Motor1E, GPIO.LOW)
+    stop1()
+    stop2()
 
 
 def forward():
     """
     Move the car forward, both wheel-1 & wheel-2 clockwise
-    Sleep 1s means both motors move 1s forward
     """
     c1()
     c2()
-    time.sleep(1)
-    stop()
+
+
+def backward():
+    """
+    Move the car backward, both wheel-1 & wheel-2 counter-clockwise
+    """
+    cc1()
+    cc2()
+
+
+def left():
+    """
+    Steer the car left, wheel-1 stop, wheel-2 clockwise
+    """
+    stop1()
+    c2()
+
+
+def right():
+    """
+    Steer the car right, wheel-1 clockwise, wheel-2 stop
+    """
+    c1()
+    stop2()
